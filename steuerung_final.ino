07bf19ca70c428f63d2a96dda18ade21d07c940a
Marius Schröter <43613156+mariusschroeter@users.noreply.github.com>
2022-11-24 13:08:30 +0100

    Add files via upload

--- steuerung_final.ino
+++ steuerung_final.ino
@@ -1,119 +1,115 @@
-/**
- * Link zum Github-Repository:
- * https://github.com/mariusschroeter/ladegeraet-steuerung
- */
-
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
-// Bibliotheken um sich zum Wifi zu verbinden +
-// einen get Request ausführen zu können
 HTTPClient sender;
 WiFiClient wifiClient;
 
 // WLAN-Daten
-const char *ssid = "";               // Wifi-Name
-const char *password = "";           // Wifi-Passwort
-const unsigned int LED_PIN = 5;      // GPIO Pin-Nummer
-const unsigned int DELAY_TIME = 5;   // Millisekunden
-const unsigned int MIN_LEVEL = 0;    // Minimum-Wert
-const unsigned int MAX_LEVEL = 1023; // Maximum-Wert
-unsigned int CURRENT_VALUE = 0;      // Aktueller Wert
+const char* ssid = "";
+const char* password = "";
+const unsigned int LED_PIN    = 5; // GPIO pin number
+const unsigned int DELAY_TIME = 5; // Milliseconds
+const unsigned int MIN_LEVEL  = 0;
+const unsigned int MAX_LEVEL  = 255; //1023
+unsigned int CURRENT_VALUE = 0;
 
-void setup()
-{
-  // Legt die Datenrate in Bit pro Sekunde (Baud) für die serielle Datenübertragung fest.
+void setup() {
   Serial.begin(115200);
-
-  // Zum Wifi verbinden
+  
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(200);
     Serial.print(".");
   }
-
   Serial.println("Verbunden!");
-
-  // Konfiguriert den spezifizierten Pin als Input oder Output.
-  pinMode(LED_PIN, OUTPUT);
+ pinMode(LED_PIN,OUTPUT); // Set LED pin as output
 }
 
-void loop()
-{
-  const char dataUrl = "http://192.168.0.55/data";
-  if (sender.begin(wifiClient, dataUrl))
-  {
+void loop() {
+  //läuft nach setup
+  if (sender.begin(wifiClient, "http://192.168.0.55/data")) {
 
-    // HTTP-Code der Response speichern
-    int httpCode = sender.GET();
+      // HTTP-Code der Response speichern
+      int httpCode = sender.GET();
 
-    if (httpCode > 0)
-    {
+      if (httpCode > 0) {
+        
+        // Anfrage wurde gesendet und Server hat geantwortet
+        // Info: Der HTTP-Code für 'OK' ist 200
+        if (httpCode == HTTP_CODE_OK) {
 
-      // Anfrage wurde gesendet und Server hat geantwortet
-      // Info: Der HTTP-Code für 'OK' ist 200
-      if (httpCode == HTTP_CODE_OK)
-      {
+          // Hier wurden die Daten vom Server empfangen
 
-        // Hier wurden die Daten vom Server empfangen
+          // String vom Webseiteninhalt speichern
+          String payload = sender.getString();
 
-        // String vom Webseiteninhalt speichern
-        String payload = sender.getString();
+          // Parse response
+          DynamicJsonDocument doc(2048);
+          deserializeJson(doc, payload);
 
-        // Parse response
-        DynamicJsonDocument doc(2048);
-        deserializeJson(doc, payload);
+          //get totalPower=tP from json obj doc
+          float tP = doc["L1L2L3"];
+          
+          Serial.println("Total Power");
+          Serial.println(tP);
 
-        // tP = totalPower aus dem Json Objekt (doc)
-        float tP = doc["L1L2L3"];
+          Serial.println("Aktueller Wert");
+          Serial.println(CURRENT_VALUE);
 
-        unsigned int i = CURRENT_VALUE;
+          unsigned int i = CURRENT_VALUE;
 
-        // Fall1: totalPower kleiner gleich -50
-        if (tP <= -50)
-        {
-          if (i < MAX_LEVEL)
-          {
-            unsigned int j = i + 100;
-            for (i; i < j; i++)
-            {
-              analogWriteDelay(LED_PIN, i, DELAY_TIME);
-              CURRENT_VALUE = j;
+          unsigned int j;
+
+          if(tP <= -50){ // 1 --- (+10)
+            if(i <= 255){
+              j = i + 10;
+              for(i; i < j; i++) // [0,255]
+                analogWriteDelay(LED_PIN,i,DELAY_TIME);
+                CURRENT_VALUE = j;
+            }
+          } else if(tP < 0 && tP > -50){ // 2 --- (-10) 
+            j = i - 10;
+            if(i < 10){
+              j = 0; 
+            }
+            if(i != 0){
+              for(i; i > j; i--){
+                  analogWriteDelay(LED_PIN,i,DELAY_TIME);
+                  CURRENT_VALUE = j;
+              }
+            }
+          } else { // 3 --- (auf 0)
+            j = 0;
+            if(i != 0){
+              for(i; i > j; i--){
+                  analogWriteDelay(LED_PIN,i,DELAY_TIME);
+                  CURRENT_VALUE = j;
+              }
             }
           }
-        }
-        // Fall2: totalPower größer gleich -45
-        if (tP >= -45)
-        {
-          for (i; i > MIN_LEVEL; i--)
-          {
-            analogWriteDelay(LED_PIN, i, DELAY_TIME);
-            CURRENT_VALUE = i;
-          }
-        }
-      }
-      else
-      {
+         
+          Serial.println("--- Durchlauf ende ---");
+        
+      }else{
         // Falls HTTP-Error
         Serial.printf("HTTP-Error: ", sender.errorToString(httpCode).c_str());
       }
-    }
-    else
-    {
+
+    }else {
       Serial.printf("HTTP-Verbindung konnte nicht hergestellt werden!");
+    
+      
     }
 
     delay(2000);
   }
 }
 
-void analogWriteDelay(unsigned int pin,
-                      unsigned int value,
-                      unsigned int waitTime)
-{
-  analogWrite(pin, value); // Schreibt einen PWM-Wellen-Wert (value) auf einen Pin (pin)
-  delay(waitTime);         // Warten für waitTime (Millisekunden)
+void analogWriteDelay(unsigned int pin, 
+                      unsigned int value, 
+                      unsigned int waitTime) {
+    analogWrite(pin,value); // Analog write in LED pin
+    delay(waitTime);        // Delay in milliseconds  
 }
